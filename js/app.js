aea1241a690b0e05df9536e9415878410cdd7264
Jerald Tonmoy Dias <59910969+Jerald-tonmoy-dias@users.noreply.github.com>
2022-07-20 17:07:32 +0600

    stepper plugin

--- js/app.js
+++ js/app.js
@@ -1,8 +1,27 @@
-const apiGenerateBtn = document.getElementById("copyGeneratBtn");
-const copyApiText = document.getElementById("copyGeneratedApi");
+// const apiGenerateBtn = document.getElementById("copyGeneratBtn");
+// const copyApiText = document.getElementById("copyGeneratedApi");
 
-apiGenerateBtn.addEventListener("click", () => {
-  let result = copyApiText.innerText;
-  copyTextToClipboard(result);
-  console.log(result);
-});
+// apiGenerateBtn.addEventListener("click", () => {
+//   let result = copyApiText.innerText;
+//   copyTextToClipboard(result);
+//   console.log(result);
+// });
+
+// customer stepper js
+
+// stepper_progress
+let stepper_progress = document.getElementById("stepper_progress");
+let stepper_wrapper = document.getElementById("stepper_wrapper");
+let childItem = document.querySelectorAll(".stepper_item");
+
+// functions
+let customProgressFunc = (percentage, itemToAdd) => {
+  stepper_progress.style.width = `${percentage}%`;
+
+  //   code here
+  for (let i = 0; i < childItem.length; i++) {
+    childItem[i].classList.add("hidden");
+  }
+  document.getElementById(`${itemToAdd}`).classList.remove("hidden");
+  //   code here
+};
