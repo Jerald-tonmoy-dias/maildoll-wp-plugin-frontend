a9fc3b64c0bc4341b3a6081a2d792780cf4d5c41
Jerald Tonmoy Dias <59910969+Jerald-tonmoy-dias@users.noreply.github.com>
2022-07-21 16:08:36 +0600

    nested tab

--- js/app.js
+++ js/app.js
@@ -1,43 +1,37 @@
-// const apiGenerateBtn = document.getElementById("copyGeneratBtn");
-// const copyApiText = document.getElementById("copyGeneratedApi");
+$(document).ready(function () {
+  // nice select
+  $("select").niceSelect();
 
-// apiGenerateBtn.addEventListener("click", () => {
-//   let result = copyApiText.innerText;
-//   copyTextToClipboard(result);
-//   console.log(result);
-// });
+  // stepper_progress
+  // let stepper_wrapper = document.getElementById("stepper_wrapper");
+  let stepper_progress = document.getElementById("stepper_progress");
+  let childItem = document.querySelectorAll(".stepper_item");
 
-// customer stepper js
+  // functions
+  let customProgressFunc = (percentage, itemToAdd) => {
+    // percentage
+    stepper_progress.style.width = `${percentage}%`;
+    // stepper_progress.classList.add("bg-violate-800");
 
-// stepper_progress
-// let stepper_wrapper = document.getElementById("stepper_wrapper");
-let stepper_progress = document.getElementById("stepper_progress");
-let childItem = document.querySelectorAll(".stepper_item");
+    // code here
+    for (let i = 0; i < childItem.length; i++) {
+      childItem[i].classList.add("hidden");
+    }
+    document.getElementById(`${itemToAdd}`).classList.remove("hidden");
+    // code here
+  };
 
-// functions
-let customProgressFunc = (percentage, itemToAdd) => {
-  // percentage
-  stepper_progress.style.width = `${percentage}%`;
-  // stepper_progress.classList.add("bg-violate-800");
+  // prev function
+  let prevProgressFunc = (percentage, prevItem, thisItem) => {
+    stepper_progress.style.width = `${percentage}%`;
+    document.getElementById(`${thisItem}`).classList.add("hidden");
+    document.getElementById(`${prevItem}`).classList.remove("hidden");
+  };
 
-  // code here
-  for (let i = 0; i < childItem.length; i++) {
-    childItem[i].classList.add("hidden");
-  }
-  document.getElementById(`${itemToAdd}`).classList.remove("hidden");
-  // code here
-};
-
-// prev function
-let prevProgressFunc = (percentage, prevItem, thisItem) => {
-  stepper_progress.style.width = `${percentage}%`;
-  document.getElementById(`${thisItem}`).classList.add("hidden");
-  document.getElementById(`${prevItem}`).classList.remove("hidden");
-};
-
-// next function
-let nextProgressFunc = (percentage, prevItem, thisItem) => {
-  stepper_progress.style.width = `${percentage}%`;
-  document.getElementById(`${thisItem}`).classList.add("hidden");
-  document.getElementById(`${prevItem}`).classList.remove("hidden");
-};
+  // next function
+  let nextProgressFunc = (percentage, prevItem, thisItem) => {
+    stepper_progress.style.width = `${percentage}%`;
+    document.getElementById(`${thisItem}`).classList.add("hidden");
+    document.getElementById(`${prevItem}`).classList.remove("hidden");
+  };
+});
